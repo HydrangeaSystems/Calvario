3d30b667f8f166531efaf13658e079dc11c7e822
merumerutho <merutochan@gmail.com>
2024-09-02 22:44:03 +0200

    Makes much more sense this way :) Output control is now mix (dry/wet) Better Heavy mode Better gain staging

--- src/Calvario.cpp
+++ src/Calvario.cpp
@@ -2,6 +2,9 @@
 #include <string>
 
 #define MAX_INT32 0x7FFFFFFF
+#define _5_OVER_MAX_INT32 2.32830643762289846205e-9
+#define NORMALIZE_10VPP(x) (x*0.2f)
+
 #define _HP 5.08
 #define _3U 128.5
 
@@ -10,7 +13,7 @@ struct Calvario : Module {
 		GAIN_SWITCH,
 		PARAM_GAIN_IN1,
 		PARAM_GAIN_IN2,
-		PARAM_GAIN_OUT,
+		PARAM_MIX_OUT,
 		PARAMS_LEN
 	};
 	enum InputId {
@@ -29,12 +32,11 @@ struct Calvario : Module {
 		LIGHTS_LEN
 	};
 
-
 	Calvario() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(PARAM_GAIN_IN1, -1.f, 1.f, 0.f, "Gain - Input1");
-		configParam(PARAM_GAIN_IN2, -1.f, 1.f, 0.f, "Gain - Input2");
-		configParam(PARAM_GAIN_OUT, 0.f, 1.f, 0.5f, "Gain - Output");
+		configParam(PARAM_GAIN_IN1, 0.f, 1.f, 0.f, "Gain - Input1");
+		configParam(PARAM_GAIN_IN2, 0.f, 1.f, 0.f, "Gain - Input2");
+		configParam(PARAM_MIX_OUT, 0.f, 1.f, 0.5f, "Mix");
 
 		configInput(SIGNAL_GAIN_MOD_IN1, "CV Gain - Input1");
 		configInput(SIGNAL_GAIN_MOD_IN2, "CV Gain - Input2");
@@ -47,27 +49,44 @@ struct Calvario : Module {
 	}
 
 	void process(const ProcessArgs& args) override {
-	    // Input gain staging
-	    float attenuate = 0.1f + (0.4f * params[GAIN_SWITCH].getValue());
 	    
-		float gain1 = (params[PARAM_GAIN_IN1].getValue() + math::clamp(inputs[SIGNAL_GAIN_MOD_IN1].getVoltage(), -5.f, 5.f) * 0.2f) * attenuate;
-		float gain2 = (params[PARAM_GAIN_IN2].getValue() + math::clamp(inputs[SIGNAL_GAIN_MOD_IN2].getVoltage(), -5.f, 5.f) * 0.2f) * attenuate;
-	    
-	    float in1 = math::clamp(inputs[SIGNAL_INPUT_IN1].getVoltage(), -5.f, 5.f) * gain1;
-	    float in2 = math::clamp(inputs[SIGNAL_INPUT_IN2].getVoltage(), -5.f, 5.f) * gain2; 
+		// Input gain is ((Knob + normalized CV) * mode_gain)
+		float gain1 = (params[PARAM_GAIN_IN1].getValue() + NORMALIZE_10VPP(math::clamp(inputs[SIGNAL_GAIN_MOD_IN1].getVoltage(), -5.f, 5.f))) * 0.2f;
+		float gain2 = (params[PARAM_GAIN_IN2].getValue() + NORMALIZE_10VPP(math::clamp(inputs[SIGNAL_GAIN_MOD_IN2].getVoltage(), -5.f, 5.f))) * 0.2f;
+		
+		// Input signals
+		float in_signal1 = inputs[SIGNAL_INPUT_IN1].getVoltage();
+		float in_signal2 = inputs[SIGNAL_INPUT_IN2].getVoltage();
+		
+		// Save dry signal 1
+		float dry_signal1 = in_signal1;
+		
+		// Limiter @0dB (10Vpp) => Normalize 10 Vpp to [-1.0 ; 1.0]
+	    in_signal1 = NORMALIZE_10VPP(math::clamp(in_signal1, -5.f, 5.f) );
+	    in_signal2 = NORMALIZE_10VPP(math::clamp(in_signal2, -5.f, 5.f) );
+		
+		// Apply gains
+		in_signal1 *= gain1;
+		in_signal2 *= gain2;
 
 	    // Convert to int
-	    int in1_int = (int) ((MAX_INT32) * in1);
-        int in2_int = (int) ((MAX_INT32) * in2);
+	    int in1_toXor = (int) ((MAX_INT32) * in_signal1);
+        int in2_toXor = (int) ((MAX_INT32) * in_signal2);
 
         // Apply XOR
-        int xorred = (in1_int ^ in2_int);
+        int xor_result = (in1_toXor ^ in2_toXor);
+        // Bitshift for extra crispiness
+        xor_result <<= (int(params[GAIN_SWITCH].getValue() * 2) + 2);
 
         // Convert to float and output gain staging
-        float xorred_float = 10.f * params[PARAM_GAIN_OUT].getValue() * ((float)xorred / MAX_INT32);
-
-        // Limiter at 0 dB (not sure if needed)
-        float output = math::clamp(xorred_float, -5.f, 5.f);
+        float xor_toOut = ((float)xor_result * _5_OVER_MAX_INT32);
+		
+        // Mix (IN1 + XOR) 
+		float mix = params[PARAM_MIX_OUT].getValue();
+        float output = (dry_signal1*(1.0f - mix) + xor_toOut*mix);
+		
+		// Apply Limiter @0dB (10 Vpp)
+		output = math::clamp(output, -5.f, 5.f);
         outputs[SIGNAL_OUTPUT].setVoltage(output);
 
         // Blink light
@@ -75,31 +94,35 @@ struct Calvario : Module {
 	}
 };
 
-
 struct CalvarioWidget : ModuleWidget {
 		CalvarioWidget(Calvario* module) {
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/calvario.svg")));
 
+		// Screws
 		//addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		//addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
+		// Input Signal 1
 		addParam(createParamCentered<Trimpot>(mm2px(Vec(_HP, 11)), module, Calvario::PARAM_GAIN_IN1));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(_HP, 19)), module, Calvario::SIGNAL_GAIN_MOD_IN1));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(_HP, 29)), module, Calvario::SIGNAL_INPUT_IN1));
 		
-		
+		// Input Signal 2
 		addParam(createParamCentered<Trimpot>(mm2px(Vec(_HP, 41)), module, Calvario::PARAM_GAIN_IN2));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(_HP, 49)), module, Calvario::SIGNAL_GAIN_MOD_IN2));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(_HP, 59)), module, Calvario::SIGNAL_INPUT_IN2));
 		
+		// Mode Switch
 		addParam(createParamCentered<CKSS>(mm2px(Vec(_HP, 71)), module, Calvario::GAIN_SWITCH));
 
-		addParam(createParamCentered<Trimpot>(mm2px(Vec(_HP, 85)), module, Calvario::PARAM_GAIN_OUT));
+		// Output Signal
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(_HP, 85)), module, Calvario::PARAM_MIX_OUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(_HP, 95)), module, Calvario::SIGNAL_OUTPUT));
 		
+		// LED
 		addChild(createLightCentered<TinyLight<RedLight>>(mm2px(Vec(_HP, .9*_3U+2.4)), module, Calvario::BLINK_LIGHT));
 	}
 };
